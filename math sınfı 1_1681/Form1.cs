08545467e8cdc967ed54cecda9b79d7ef7ab4b96
denizsu34 <148059058+denizsu34@users.noreply.github.com>
2023-12-18 11:17:17 +0300

    Add files via upload

---
+++ math sınfı 1_1681/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace math_sınfı_1_1681
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double sayi =Convert.ToSingle(txtSayi.Text);//sayisal olarak texti çevirdik
+
+            //işlemler
+            double sayiMutlak = Math.Abs(sayi);
+            double sayiUs = Math.Pow(sayi, 2); //txt içindeki sayının karesi (2. kuvveti)
+            double sayıkarekok = Math.Sqrt(sayi);
+            double sayıYukarıYuvarla = Math.Ceiling(sayi);
+            double sayıAsagiYuvarla = Math.Floor(sayi);
+            double sayıYuvarla = Math.Round(sayi);
+            //round ,floor ,ceil
+            
+
+
+
+
+            //somuçları yazdır
+            lblMutlak.Text = "sayının mutlak değeri :" + sayiMutlak.ToString();
+            lblUst.Text = "sayının kuvveti :"+sayiUs.ToString();
+            lblKok.Text ="sayının kar kökü :"+ sayıkarekok.ToString();
+            lblYukariYuvarla.Text ="sayının Yukarı Yuvarla :" + sayıYukarıYuvarla.ToString();
+            lblAsagıYuvarla.Text = "sayı Aşağı Yuvarla:" + sayıAsagiYuvarla.ToString();
+            lblYuvarla.Text = "sayı Yuvarla:" + sayıYuvarla.ToString();
+
+
+        }
+
+        private void txtSayi_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ math sınfı 1_1681/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace math_sınfı_1_1681
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
